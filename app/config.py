22677af7b54402c8ed41dd3db3ebe5dbf3817d93
vlkttlv <vitavitavita04@bk.ru>
2025-01-18 17:12:27 +0700

    config)))

---
+++ app/config.py
@@ -0,0 +1,36 @@
+from typing import Literal
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    MODE: Literal["DEV", 'TEST', 'PROD']
+    
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    # TEST_DB_HOST: str
+    # TEST_DB_PORT: int
+    # TEST_DB_USER: str
+    # TEST_DB_PASS: str
+    # TEST_DB_NAME: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    # @property
+    # def TEST_DATABASE_URL(self):
+    #     return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+
+    class Config:  # настройки будут загружаться из файла .env
+        env_file = ".env"
+
+
+settings = Settings()  # содержит значения настроек из файла .env
