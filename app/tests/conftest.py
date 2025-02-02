6524e73acdbd0837f49a1c929f7335b143c6fe77
vlkttlv <vitavitavita04@bk.ru>
2025-02-02 23:16:34 +0700

    conftest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,4 @@
-import asyncio
+from datetime import datetime
 import json
 from sqlalchemy import insert
 import pytest
@@ -13,8 +13,6 @@ from app.shelduled_workouts.models import ShelduledWorkout
 from httpx import AsyncClient
 
 from app.main import app as fastapi_app
-# фикстура - функция, котор-я подготавливает среду для тестирования
-
 
 @pytest.fixture(autouse=True, scope="session")
 async def prepare_database():
@@ -24,30 +22,35 @@ async def prepare_database():
         await conn.run_sync(Base.metadata.create_all)
 
     def open_test_json(model: str):
-        with open(f"app/tests/test_{model}.json", "r", encoding='utf-8 ') as file:
+        with open(f"app/tests/mock_data/test_{model}.json", "r", encoding='utf-8 ') as file:
             return json.load(file)
 
-    # hotels = open_test_json("hotels")
-    # rooms = open_test_json("rooms")
     users = open_test_json("users")
     exercises = open_test_json("exercises")
-    # bookings = open_test_json("bookings")
-    # payments = open_test_json("payments")
+    workout_plans = open_test_json("workout_plans")
+    workout_exercises = open_test_json("workout_exercises")
+    shelduled_workouts = open_test_json("shelduled_workouts")
 
-    # for booking in bookings:
-    #     booking["date_from"] = datetime.strptime(
-    #         booking["date_from"], "%Y-%m-%d")
-    #     booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
+    for workout_plan in workout_plans:
+        workout_plan["created_at"] = datetime.strptime(workout_plan["created_at"], '%Y-%m-%d %H:%M:%S')
 
-    # for payment in payments:
-    #     payment["date_to"] = datetime.strptime(payment["date_to"], "%Y-%m-%d")
+    for shelduled_workout in shelduled_workouts:
+        shelduled_workout["shelduled_date"] = datetime.strptime(shelduled_workout["shelduled_date"], '%Y-%m-%d')
+        shelduled_workout["shelduled_time"] = datetime.strptime(shelduled_workout["shelduled_time"], '%H:%M:%S')
+
 
     async with async_session_maker() as session:
         add_users = insert(Users).values(users)
         add_exercises = insert(Exercise).values(exercises)
+        add_workout_plans = insert(WorkoutPlans).values(workout_plans)
+        add_workout_exercises = insert(WorkoutExercise).values(workout_exercises)
+        add_shelduled_workouts = insert(ShelduledWorkout).values(shelduled_workouts)
 
         await session.execute(add_users)
         await session.execute(add_exercises)
+        await session.execute(add_workout_plans)
+        await session.execute(add_workout_exercises)
+        await session.execute(add_shelduled_workouts)
 
         await session.commit()
 
@@ -60,7 +63,7 @@ async def ac():
 @pytest.fixture(scope="session")
 async def auth_ac():
     async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
-        await ac.post("auth/login", json={"email": "test@test.com", "password": "password"})
+        await ac.post("auth/login", json={"email": "vvb63@tpu.ru", "password": "password"})
         assert ac.cookies["access_token"]
         yield ac
 
