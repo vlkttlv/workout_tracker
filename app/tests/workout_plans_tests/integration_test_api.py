04faa0618165aaa57400e27aef7da01b3d479711
vlkttlv <vitavitavita04@bk.ru>
2025-02-02 23:15:45 +0700

    add workout plans tests

---
+++ app/tests/workout_plans_tests/integration_test_api.py
@@ -0,0 +1,29 @@
+"""httpx предоставляет асинхронного клиента для тестов"""
+from httpx import AsyncClient
+import pytest
+from app.tests.workout_plans_tests.mock_data import mock_workout_plan
+
+@pytest.mark.asyncio(scope="session")
+async def test_get_workout_plan(auth_ac: AsyncClient):
+    """Тест, проверяющий регистрацию пользователя"""
+    response = await auth_ac.get("/workout_plan/")
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+    response = await auth_ac.get("/workout_plan/1")
+    assert response.status_code == 200
+    response = await auth_ac.get("/workout_plan/4")
+    assert response.status_code == 409
+
+@pytest.mark.asyncio(scope="session")
+async def test_add_and_delete_workout_plan(auth_ac: AsyncClient):
+    """Тест, проверяющий регистрацию пользователя"""
+    response = await auth_ac.post("/workout_plan/", json=mock_workout_plan)
+    assert response.status_code == 201
+    response = await auth_ac.get("/workout_plan/")
+    assert response.status_code == 200
+    assert len(response.json()) == 4
+    response = await auth_ac.delete("/workout_plan/4")
+    assert response.status_code == 200
+    response = await auth_ac.get("/workout_plan/")
+    assert response.status_code == 200
+    assert len(response.json()) == 3
---
+++ app/tests/workout_plans_tests/mock_data.py
@@ -0,0 +1,10 @@
+mock_workout_plan = {"plan": {"name": "тестовое название",
+                              "description": "тестовое описание"
+                              },
+                              "exercises": [{
+                                  "exercise_id": 1,
+                                  "reps": 1,
+                                  "sets": 1,
+                                  "weight": 1
+                                  }]
+                    }
