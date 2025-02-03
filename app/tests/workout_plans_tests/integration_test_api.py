ed35779eea7f537884ad085ab4161ba062f03e34
vlkttlv <vitavitavita04@bk.ru>
2025-02-03 15:54:03 +0700

    commit

--- app/tests/workout_plans_tests/integration_test_api.py
+++ app/tests/workout_plans_tests/integration_test_api.py
@@ -12,7 +12,7 @@ async def test_get_workout_plan(auth_ac: AsyncClient):
     response = await auth_ac.get("/workout_plan/1")
     assert response.status_code == 200
     response = await auth_ac.get("/workout_plan/4")
-    assert response.status_code == 409
+    assert response.json() == []
 
 @pytest.mark.asyncio(scope="session")
 async def test_add_and_delete_workout_plan(auth_ac: AsyncClient):
@@ -22,7 +22,7 @@ async def test_add_and_delete_workout_plan(auth_ac: AsyncClient):
     response = await auth_ac.get("/workout_plan/")
     assert response.status_code == 200
     assert len(response.json()) == 4
-    response = await auth_ac.delete("/workout_plan/4")
+    response = await auth_ac.delete("/workout_plan/5")
     assert response.status_code == 200
     response = await auth_ac.get("/workout_plan/")
     assert response.status_code == 200
