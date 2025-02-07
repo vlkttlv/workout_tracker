313e0ad4814c01bb23d6df78d55cd90055a94be1
vlkttlv <vitavitavita04@bk.ru>
2025-02-07 16:04:13 +0700

    добавлен метод update

--- app/exercises/dao.py
+++ app/exercises/dao.py
@@ -1,5 +1,7 @@
+from sqlalchemy import update, and_
 from app.dao.base import BaseDAO
 from app.exercises.models import Exercise, WorkoutExercise
+from app.db.database import async_session_maker
 
 class ExerciseDAO(BaseDAO):
 
@@ -10,3 +12,18 @@ class WorkoutExerciseDAO(BaseDAO):
 
     model = WorkoutExercise
 
+    @classmethod
+    async def update(cls, workout_plan_id: int, exercise_id: int, **data):
+        """
+        Обновляет запись
+
+        -Аргументы:
+            id: ID записи, которую надо обновить
+            **data: атрибуты модели в качестве ключей и их значения в качестве значений.
+        -Пример:
+            await WorkoutPlansDAO.update_workout_plan(workout_id=workout_id, description=update_workout.description)
+        """
+        async with async_session_maker() as session:
+            stmt = update(cls.model).where(and_(cls.model.exercise_id == exercise_id, cls.model.workout_plan_id == workout_plan_id)).values(**data)
+            await session.execute(stmt)
+            await session.commit()
